77678bc7d7eb6de851150333768074de17971cdf
omer-mar <omermar7@gmail.com>
2020-11-25 19:10:55 +0200

    liquidPump code

--- arduino_code/src/liquidPump.cpp
+++ arduino_code/src/liquidPump.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+// constants won't change
+const int RELAY_PIN = A5;  // the Arduino pin, which connects to the IN pin of relay
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin A5 as an output.
+  pinMode(RELAY_PIN, OUTPUT);
 }
-
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(RELAY_PIN, HIGH); // turn on pump 5 seconds
+  delay(5000);
+  digitalWrite(RELAY_PIN, LOW);  // turn off pump 5 seconds
+  delay(5000);
 }
