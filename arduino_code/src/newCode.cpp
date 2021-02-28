35a991fc92caf77e849a42c5e3e8e13a695f09b0
Omer MarChaim <omermar7@gmail.com>
2021-02-28 13:41:45 +0200

    ADD AS726X FUNC
    ADD AS726X FUNC

--- arduino_code/src/newCode.cpp
+++ arduino_code/src/newCode.cpp
@@ -3,11 +3,14 @@
 #include <SD.h>
 #include <SPI.h>
 #include <string.h>
+#include "/lib/SparkFun_AS726X/src/AS726X.h"
 
 #define STATE_NONE 0
 #define STATE_GROWING 10
 #define STATE_HARVESTING 20
 #define STATE_FEEDING 30
+
+AS726X sensor;
 
 int handleGrowing();
 int handleHarvesting();
@@ -17,12 +20,18 @@ void logToFile(String msg);
 int restorePreviousState();
 void logToFile(String);
 
+//varabales we want to adjust
 File data_file;
 int newState = STATE_NONE;
 int state;
 int harvesteTimeDelay = 5000;
 int feedingTimeDelay = 5000;
 int airPressure = 40;
+int goalGreenValue = 0;         //need to update
+int LightSampleInterval = 600000; // ten minuts
+unsigned long startMillis;        //start time of the program
+unsigned long currentMillis;      // update every call to millis()
+
 // t_time lastStateTransition;
 const int SD_pin = 10;
 const int PUMP1 = 6;
@@ -38,9 +47,10 @@ void setup()
   pinMode(PUMP1, OUTPUT);
   pinMode(PUMP2, OUTPUT);
   pinMode(AIR, OUTPUT);
-  EEPROM.write(202, harvesteTimeDelay);
+  EEPROM.write(202, harvesteTimeDelay); // set details to eeprom
   EEPROM.write(303, harvesteTimeDelay);
   data_file = SD.open("data.txt", FILE_WRITE);
+  startMillis = millis(); //initial start time
 }
 
 void loop()
@@ -66,25 +76,52 @@ void loop()
 
 long lastSpectometerCheckTstamp;
 
+// 1. check how much time passed since growing state started - for detecting HW problems or fatalities
+// 2. check (millis() - lastSpectometerCheckTstamp):
+// 2.1 if more than  10 minutes - check, logToFile() and update lastSpectometerCheckTstamp = millis();
+// 2.1.1 if spectometer green enough, return STATE_HARVESTING
+// 2.2 if not, return STATE_GROWING
+
+
 int handleGrowing()
 {
-  // TODO:
-  // 1. check how much time passed since growing state started - for detecting HW problems or fatalities
-  // 2. check (millis() - lastSpectometerCheckTstamp):
-  // 2.1 if more than  10 minutes - check, logToFile() and update lastSpectometerCheckTstamp = millis();
-  // 2.1.1 if spectometer green enough, return STATE_HARVESTING
-  // 2.2 if not, return STATE_GROWING
+
+  currentMillis = millis();
+  if (currentMillis - startMillis > LightSampleInterval) // cheack if we waiting more then LightSampleInterval
+  {
+   sensor.takeMeasurementsWithBulb(); //take sensor sample
+    float sensorSample = sensor.getCalibratedOrange();
+    Serial.print("Sampling light: green value is ");
+    Serial.print(sensorSample);
+    logToFile(String("the green value is: ") + sensorSample); //write to log file
+    if (sensorSample > goalGreenValue)
+    {
+      startMillis = currentMillis;
+      return STATE_HARVESTING;
+    }
+    Serial.println("Air Pump Off"); // give one minute rest for the air pump
+    logToFile("air pumped off");
+    while (millis() < currentMillis + 60000) // 60000= one mimute
+    {
+      analogWrite(AIR, 0);
+    }
+    analogWrite(AIR, airPressure); //Turn airPump On
+    Serial.println("Air Pump On");
+    logToFile("air pumped on");
+    startMillis = currentMillis;
+  }
 
   return STATE_GROWING;
 }
 
+// activate harvesting pump for preconfigued time
+
 int handleHarvesting()
 {
-  // TODO: activate harvesting pump for preconfigued time
   int harvestingDurationSeconds = EEPROM.read(202);
-  analogWrite(AIR, 0);              //Turn airPump Off
+  analogWrite(AIR, 0); //Turn airPump Off
   Serial.println("Air Pump Off");
-  digitalWrite(PUMP2, HIGH);        // activate pump2
+  digitalWrite(PUMP2, HIGH); // activate pump2
   Serial.println("Pump2 Pump On");
   delay(harvestingDurationSeconds); // delay(harvestingDurationSeconds * 1000L);
   analogWrite(AIR, airPressure);    //Turn airPump On
@@ -94,17 +131,18 @@ int handleHarvesting()
   return STATE_FEEDING;             //next step State
 }
 
+ //  activate feeding pump for preconfigued time
 int handleFeeding()
 {
-  // TODO: activate feeding pump for preconfigued time
+
   int feedingDurationSeconds = EEPROM.read(303);
-  digitalWrite(PUMP1, HIGH);         // activate pump1
+  digitalWrite(PUMP1, HIGH); // activate pump1
   Serial.println("Pump1 Pump On");
   delay(feedingDurationSeconds);
   digitalWrite(PUMP1, LOW);         // stop activate pump1
   Serial.println("Pump1 Pump Off"); // delay(feedingDurationSeconds * 1000L);
 
-  return STATE_GROWING;             //next step State
+  return STATE_GROWING; //next step State
 }
 
 void handleStateTransition(int old, int neww)
@@ -112,7 +150,6 @@ void handleStateTransition(int old, int neww)
   logToFile(String("Old State: ") + old + ", new state: " + neww);
   // 1. persist new state to EEPROM: which, start time
   EEPROM.write(0, neww);
-  // TODO:
   // 1.1. get current time using rtc.now();
   // 1.2. serialize time to bytes
   // 1.3. persist times bytes to eeprom at address 1 upwards
@@ -130,8 +167,10 @@ void logToFile(String msg)
 {
   if (data_file)
   {
-    data_file.println(msg);       // cheack how to right rtc.now
-   // data_file.println(rtc.now()); // cheack how to wright rtc.now to show exact time
+
+    data_file.println(msg); // check how to write rtc.now
+    // String time = getTime();         //need to implement
+    // data_file.println("At" + time); // data_file.println(rtc.now()); // cheack how to wright rtc.now to show exact time
   }
   else
   {
